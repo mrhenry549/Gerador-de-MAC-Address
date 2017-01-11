ccac7ad78efffef00e26e3160521d903ef4af9f0
Henrique <Henrique@Henrique-HP>
2017-01-11 19:40:56 +0000

    fim

--- Gerador de MAC Address/src/gerador/de/mac/address/Gerador.java
+++ Gerador de MAC Address/src/gerador/de/mac/address/Gerador.java
@@ -5,23 +5,19 @@ import java.util.Random;
 public class Gerador {
     
     String[] hexadecimal, macValues;
-    String macValue1, macValue2;
     int par1, par2;
     Random rand;
     
     public Gerador(){
         hexadecimal = new String[]{"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"};
-        macValues = new String[]{};
+        macValues = new String[6];
         rand = new Random();
         
         for(int i = 0; i < 6; i++){
-            par1 = rand.nextInt(hexadecimal.length - 1);
-            par2 = rand.nextInt(hexadecimal.length - 1);
+            par1 = rand.nextInt(hexadecimal.length);
+            par2 = rand.nextInt(hexadecimal.length);
             
-            macValue1 = hexadecimal[par1];
-            macValue2 = hexadecimal[par2];
-            
-            macValues[i] = macValue1+macValue2;
+            macValues[i] = hexadecimal[par1]+hexadecimal[par2];
         }
         
         System.out.println("MAC Address gerado: "+macValues[0]+":"+macValues[1]+":"+macValues[2]+":"+macValues[3]+":"+macValues[4]+":"+macValues[5]);
--- Gerador de MAC Address/src/gerador/de/mac/address/MACAddressGen.java
+++ Gerador de MAC Address/src/gerador/de/mac/address/MACAddressGen.java
@@ -3,23 +3,6 @@ package gerador.de.mac.address;
 public class MACAddressGen {
     
     public static void main(String[] args) {
-    /*Random rand = new Random();
-    byte[] macAddr = new byte[6];
-    rand.nextBytes(macAddr);
-
-    macAddr[0] = (byte)(macAddr[0] & (byte)254);  //zeroing last 2 bytes to make it unicast and locally adminstrated
-
-    StringBuilder sb = new StringBuilder(18);
-    for(byte b : macAddr){
-
-        if(sb.length() > 0)
-            sb.append(":");
-
-        sb.append(String.format("%02x", b));
-    }
-    
-    System.out.println(sb.toString());*/
-    
-    Gerador g = new Gerador();
+        Gerador g = new Gerador();
     }
 }
